f0c202eb4f2c15fa7519510ef6e69bf7451223b2
Daniel Bolivar <daboliva@gmail.com>
2018-04-09 14:26:58 +0200

    more tictac

--- katas/tictac/__tests__/tictac.test.js
+++ katas/tictac/__tests__/tictac.test.js
@@ -1,6 +1,10 @@
+const cloneArray = array => JSON.parse(JSON.stringify(array));
+
 const placeMove = type => (position, field) => {
-  if (type === "X") return [[0, 0, "X"], [0, 0, 0], [0, 0, 0]];
-  return [["O", 0, 0], [0, 0, 0], [0, 0, 0]];
+  const newField = cloneArray(field);
+  if (newField[position[0]][position[1]] !== 0) return newField;
+  newField[position[0]][position[1]] = type;
+  return newField;
 };
 
 const placeX = placeMove("X");
@@ -8,17 +12,30 @@ const placeX = placeMove("X");
 const placeO = placeMove("O");
 
 describe("Tic Tac Toe", () => {
+  const emptyField = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
   it("should put an X in the top right corner on an empty Field", () => {
-    const field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     const expected = [[0, 0, "X"], [0, 0, 0], [0, 0, 0]];
-    const actual = placeX([0, 2], field);
-    expect(expected).toEqual(actual);
+    const actual = placeX([0, 2], emptyField);
+    expect(actual).toEqual(expected);
   });
 
   it("should put an O in the top left corner on an empty field", () => {
-    const field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     const expected = [["O", 0, 0], [0, 0, 0], [0, 0, 0]];
-    const actual = placeO([0, 2], field);
-    expect(expected).toEqual(actual);
+    const actual = placeO([0, 0], emptyField);
+    expect(actual).toEqual(expected);
+  });
+
+  it("should put an X in the middle when there's already an O in the top right corner", () => {
+    const field = [[0, 0, "O"], [0, 0, 0], [0, 0, 0]];
+    const expected = [[0, 0, "O"], [0, "X", 0], [0, 0, 0]];
+    const actual = placeX([1, 1], field);
+    expect(actual).toEqual(expected);
+  });
+
+  it("should not put an X where there's already an X or an O", () => {
+    const field = [[0, 0, "O"], [0, 0, 0], [0, 0, 0]];
+    const expected = [[0, 0, "O"], [0, 0, 0], [0, 0, 0]];
+    const actual = placeX([0, 2], field);
+    expect(actual).toEqual(expected);
   });
 });
